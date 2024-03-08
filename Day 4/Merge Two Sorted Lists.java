5acdc535e8792e8e15297f1a7161f82f74b2d041
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2024-03-08 20:00:01 +0530

    Create Merge Two Sorted Lists.java

---
+++ Day 4/Merge Two Sorted Lists.java
@@ -0,0 +1,64 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
+
+public class MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                current.next = l1;
+                l1 = l1.next;
+            } else {
+                current.next = l2;
+                l2 = l2.next;
+            }
+            current = current.next;
+        }
+
+        // Append remaining nodes from either list
+        if (l1 != null) {
+            current.next = l1;
+        }
+        if (l2 != null) {
+            current.next = l2;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        // Create the first sorted linked list: 1 -> 2 -> 4
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(2);
+        l1.next.next = new ListNode(4);
+
+        // Create the second sorted linked list: 1 -> 3 -> 4
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        // Merge the two sorted lists
+        MergeTwoSortedLists merger = new MergeTwoSortedLists();
+        ListNode mergedList = merger.mergeTwoLists(l1, l2);
+
+        // Print the merged list
+        printList(mergedList);
+    }
+
+    // Helper method to print the linked list
+    private static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+}
