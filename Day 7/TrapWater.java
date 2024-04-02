4fc098ce60d62a6381b3e1d8c964b97baa01f92b
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2024-04-02 17:36:24 +0530

    Create TrapWater.java

---
+++ Day 7/TrapWater.java
@@ -0,0 +1,37 @@
+public class TrapWater {
+    public int trap(int[] height) {
+        if (height == null || height.length == 0) {
+            return 0;
+        }
+
+        int left = 0, right = height.length - 1;
+        int maxLeft = 0, maxRight = 0;
+        int waterTrapped = 0;
+
+        while (left < right) {
+            if (height[left] <= height[right]) {
+                if (height[left] >= maxLeft) {
+                    maxLeft = height[left];
+                } else {
+                    waterTrapped += maxLeft - height[left];
+                }
+                left++;
+            } else {
+                if (height[right] >= maxRight) {
+                    maxRight = height[right];
+                } else {
+                    waterTrapped += maxRight - height[right];
+                }
+                right--;
+            }
+        }
+
+        return waterTrapped;
+    }
+
+    public static void main(String[] args) {
+        TrapWater trapWater = new TrapWater();
+        int[] height = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+        System.out.println("Total trapped water: " + trapWater.trap(height));
+    }
+}
